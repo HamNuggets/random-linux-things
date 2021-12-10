93ed5e4a06d63834673051671ed9dba51fbdcd7b
Basil <71855338+UwUham@users.noreply.github.com>
2021-12-10 02:57:24 +0000

    asdf

--- i3-custom-build/install.sh
+++ i3-custom-build/install.sh
@@ -9,6 +9,7 @@ git clone https://aur.archlinux.org/polybar.git
 git clone https://aur.archlinux.org/nerd-fonts-fira-code.git
 git clone https://aur.archlinux.org/ttf-material-design-icons.git
 git clone https://aur.archlinux.org/ttf-font-awesome-4.git
+git clone https://aur.archlinux.org/nerd-fonts-hermit.git
 cd polybar
 makepkg --noconfirm -si
 cd ..
@@ -21,6 +22,9 @@ cd ..
 cd ttf-font-awesome-4
 makepkg --noconfirm -si
 cd ..
+cd nerd-fonts-hermit
+makepkg --noconfirm -si
+cd ..
 echo "Done."
 echo "Applying custom build..."
 git clone https://github.com/UwUham/galaxy-rice-dots.git
@@ -31,5 +35,8 @@ cp -r * ~/.config
 mkdir -p ~/Pictures
 cd ~/Pictures 
 git clone https://github.com/UwUham/wallpapers.git
+echo "Done."
+echo "Removing working directory..."
+rm -rf ~/basili3
 echo "Done."
 echo "Reboot into your new i3 install to see your changes."
--- install-amogus-grub.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/sueperb/tasty-grubs.git
-cd tasty-grubs/themes
-sudo mv amogus /boot/grub/themes
-echo "GRUB-THEME=/boot/grub/themes/amogus/theme.txt" >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
